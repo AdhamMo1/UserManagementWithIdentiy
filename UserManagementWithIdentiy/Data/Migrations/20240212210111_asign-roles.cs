65f25157651d69131aca8981b283a189d30bc042
Adham Mohamed <github.com/AdhamMo1>
2024-02-13 00:01:28 +0200

    set-photo-login-parital

---
+++ UserManagementWithIdentiy/Data/Migrations/20240212210111_asign-roles.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+namespace UserManagementWithIdentiy.Data.Migrations
+{
+    public partial class asignroles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                            table: "Roles",
+                            columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+                            values: new object[] { Guid.NewGuid().ToString(), "User", "user".ToUpper(), Guid.NewGuid().ToString() },
+                            schema:"security"
+                );
+            migrationBuilder.InsertData(
+                            table: "Roles",
+                            columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+                            values: new object[] { Guid.NewGuid().ToString(), "Admin", "admin".ToUpper(), Guid.NewGuid().ToString() },
+                            schema: "security"
+                );
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("delete * from security.Roles");
+        }
+    }
+}
