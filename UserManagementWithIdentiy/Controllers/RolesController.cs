0440703a474ec59438228c04ddf98a0944212b96
Adham Mohamed <github.com/AdhamMo1>
2024-02-13 02:50:37 +0200

    display-Roles

---
+++ UserManagementWithIdentiy/Controllers/RolesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace UserManagementWithIdentiy.Controllers
+{
+    public class RolesController : Controller
+    {
+        
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RolesController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Index()
+        {
+            var Roles = await _roleManager.Roles.ToListAsync();
+            return View(Roles);
+        }
+    }
+}
