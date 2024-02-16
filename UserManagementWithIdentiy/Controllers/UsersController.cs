15baab0b160107d72c441283eb390c1ea715c96a
Adham Mohamed <github.com/AdhamMo1>
2024-02-16 03:04:52 +0200

    finish-roles

--- UserManagementWithIdentiy/Controllers/UsersController.cs
+++ UserManagementWithIdentiy/Controllers/UsersController.cs
@@ -34,5 +34,58 @@ namespace UserManagementWithIdentiy.Controllers
             }).ToListAsync();
             return View(users);
         }
+        [HttpGet]
+        public async Task<IActionResult> AddUser()
+        {
+            var _roles = await _roleManager.Roles.ToListAsync();
+            var viewModel = new AddUserFormViewModel()
+            {
+                Roles = _roles.Select(x => new RoleViewModel { RoleId = x.Id, RoleName = x.Name }).ToList()
+            };
+            return View("AddUser",viewModel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SaveAddUser(AddUserFormViewModel _user)
+        {
+            if (await _userManager.FindByEmailAsync(_user.Email)!=null)
+            {
+                ModelState.AddModelError("Email","Email is exist, try another...");
+                return View("AddUser",_user);
+            }
+            if(await _userManager.FindByNameAsync(_user.UserName)!=null)
+            {
+                ModelState.AddModelError("UserName", "User Name is exist, try another...");
+                return View("AddUser",_user);
+            }
+            if(!_user.Roles.Any(x=>x.Selected))
+            {
+                ModelState.AddModelError("Roles", "choose Role..");
+                return View("AddUser", _user);
+            }
+            
+            if (!ModelState.IsValid)
+            {
+                return View("AddUser", _user);
+            }
+            var user = new ApplicationUser()
+            {
+                FirstName = _user.FirstName,
+                LastName = _user.LastName,
+                Email = _user.Email,
+                UserName = _user.UserName,
+                
+            };
+            var result = await _userManager.CreateAsync(user,_user.Password);
+            if(!result.Succeeded)
+            {
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("Users",error.Description);
+                }
+            }
+            await _userManager.AddToRolesAsync(user,_user.Roles.Where(x=>x.Selected).Select(x=>x.RoleName).ToList());
+            return RedirectToAction("Index");
+        }
     }
 }
